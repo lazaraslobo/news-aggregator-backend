93b99997374cd946bff4e47c9c96d801f836a55a
lazaraslobo <lazaraslobo@gmail.com>
2024-09-08 13:18:46 +0530

    wip

--- setup.sh
+++ setup.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-# Adjust ownership and permissions for directories if running on the host system
-sudo chown -R $USER:www-data ./storage/
-sudo chown -R $USER:www-data ./bootstrap/cache/
-sudo chmod -R 775 ./storage/
-sudo chmod -R 775 ./bootstrap/cache/
-sudo mkdir -p ./storage/logs ./storage/framework/sessions ./bootstrap/cache
-sudo chown -R www-data:www-data ./storage ./bootstrap/cache
-sudo chmod -R 775 ./storage ./bootstrap/cache
-sudo chmod -R g+w ./storage/logs
-sudo chmod -R 775 ./
+# Ensure necessary directories exist
+mkdir -p ./storage/logs ./storage/framework/sessions ./bootstrap/cache
 
-#php artisan config:clear
-#php artisan cache:clear
-#php artisan view:clear
-#php artisan route:clear
+# Adjust ownership and permissions
+sudo chown -R $USER:www-data ./storage ./bootstrap/cache
+sudo chmod -R 775 ./storage ./bootstrap/cache
+
+# Specific permissions for logs
+sudo chmod -R g+w ./storage/logs
+
+sudo chmod o+w ./storage/ -R
+
+sudo chown www-data:www-data -R ./storage
+
+# Clear Laravel caches
 php artisan optimize:clear
